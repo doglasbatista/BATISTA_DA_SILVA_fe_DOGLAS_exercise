349f64b2db4a2f882f75a697a6a2f9393cc814e8
doglasbatista <doglasbatista@gmail.com>
2023-04-21 12:05:50 +0300

    feat: refactor teams page

--- src/pages/Teams.tsx
+++ src/pages/Teams.tsx
@@ -1,26 +1,9 @@
 import * as React from 'react';
-import {ListItem, Teams as TeamsList} from 'types';
+import {mapTeamsToListItems} from '../utils/teamUtils';
 import {useFetchTeams} from '../data/useFetchTeams';
 import Header from '../components/Header';
 import List from '../components/List';
 import {Container} from '../components/GlobalComponents';
-
-export const MapT = (teams: TeamsList[]) => {
-    return teams?.map(team => {
-        var columns = [
-            {
-                key: 'Name',
-                value: team.name,
-            },
-        ];
-        return {
-            id: team.id,
-            url: `/team/${team.id}`,
-            columns,
-            navigationProps: team,
-        } as ListItem;
-    });
-};
 
 const Teams = () => {
     const {data, isLoading} = useFetchTeams();
@@ -28,7 +11,7 @@ const Teams = () => {
     return (
         <Container>
             <Header title="Teams" showBackButton={false} />
-            <List items={MapT(data)} isLoading={isLoading} />
+            <List items={mapTeamsToListItems(data)} isLoading={isLoading} />
         </Container>
     );
 };
--- src/pages/__tests__/testTeams.tsx
+++ src/pages/__tests__/testTeams.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import {render} from '@testing-library/react';
-import Teams, {MapT} from '../Teams';
+import Teams from '../Teams';
 import {useFetchTeams} from '../../data/useFetchTeams';
 import Header from '../../components/Header';
 import List from '../../components/List';
+import {mapTeamsToListItems} from '../../utils/teamUtils';
 
 jest.mock('../../data/useFetchTeams');
 jest.mock('../../components/Header');
@@ -24,54 +25,61 @@ const teamsData = [
 ];
 
 describe('Teams', () => {
-    beforeEach(() => {
-        (useFetchTeams as jest.Mock).mockReturnValue({
-            data: teamsData,
-            isLoading: false,
-        });
-    });
-
-    it('renders header with title', () => {
-        render(<Teams />);
-
-        expect(HeaderMock).toHaveBeenCalledTimes(1);
-        expect(HeaderMock).toHaveBeenCalledWith(
-            expect.objectContaining({
-                title: 'Teams',
-                showBackButton: false,
-            }),
-            {}
-        );
-    });
-
-    it('renders teams list', () => {
-        render(<Teams />);
-
-        expect(ListMock).toHaveBeenCalledTimes(1);
-        expect(ListMock).toHaveBeenCalledWith(
-            expect.objectContaining({
-                items: MapT(teamsData),
+    describe('Header', () => {
+        it('renders header with title', () => {
+            (useFetchTeams as jest.Mock).mockReturnValue({
+                data: teamsData,
                 isLoading: false,
-            }),
-            {}
-        );
+            });
+
+            render(<Teams />);
+
+            expect(HeaderMock).toHaveBeenCalledTimes(1);
+            expect(HeaderMock).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    title: 'Teams',
+                    showBackButton: false,
+                }),
+                {}
+            );
+        });
     });
 
-    it('renders spinner while loading', () => {
-        (useFetchTeams as jest.Mock).mockReturnValue({
-            data: null,
-            isLoading: true,
+    describe('List', () => {
+        it('renders teams list', () => {
+            (useFetchTeams as jest.Mock).mockReturnValue({
+                data: teamsData,
+                isLoading: false,
+            });
+
+            render(<Teams />);
+
+            expect(ListMock).toHaveBeenCalledTimes(1);
+            expect(ListMock).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    items: mapTeamsToListItems(teamsData),
+                    isLoading: false,
+                }),
+                {}
+            );
         });
 
-        render(<Teams />);
-
-        expect(ListMock).toHaveBeenCalledTimes(1);
-        expect(ListMock).toHaveBeenCalledWith(
-            expect.objectContaining({
-                items: MapT(null),
+        it('renders spinner while loading', () => {
+            (useFetchTeams as jest.Mock).mockReturnValue({
+                data: null,
                 isLoading: true,
-            }),
-            {}
-        );
+            });
+
+            render(<Teams />);
+
+            expect(ListMock).toHaveBeenCalledTimes(1);
+            expect(ListMock).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    items: mapTeamsToListItems(null),
+                    isLoading: true,
+                }),
+                {}
+            );
+        });
     });
 });
